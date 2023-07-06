95d9da706d05b00179dd362fbb8cfc1d59d46fa5
Aghogho <monorienaghogho@gmail.com>
2023-07-06 22:17:31 +0000

    2. floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Convert float to int with floor."""
+
+
+def floor(n: float) -> int:
+    """
+    Args:
+      n(float) - floating input
+    Return: int
+    """
+    return int(n)
