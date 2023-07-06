d83ac86290c5889bd8ec13aee55cbe5d80ef84d3
Aghogho <monorienaghogho@gmail.com>
2023-07-06 22:11:02 +0000

    1. concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Concatenation of strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Args:
+      str1(str) - first string
+      str2(str) - second string
+    Return: str
+    """
+    return str1 + str2
