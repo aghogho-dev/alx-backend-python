28201861d94ec48736bfe3a06141842ca493cb50
Aghogho <monorienaghogho@gmail.com>
2023-07-06 23:34:34 +0000

    11. more

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""More types."""
+from typing import Any, Mapping, Sequence, TypeVar, Union
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """More types."""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
