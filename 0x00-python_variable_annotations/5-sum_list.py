644d73e7f9af44586cc8718f563df0b0af1e678e
Aghogho <monorienaghogho@gmail.com>
2023-07-06 22:36:00 +0000

    5. list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Use the typing libr."""
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Args:
+      input_list(list of floats)
+    Return: sum as float
+    """
+    return sum(input_list)
