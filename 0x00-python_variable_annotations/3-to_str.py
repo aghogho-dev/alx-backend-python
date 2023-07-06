05e6302885fee307e30d67bc5b348781bfd91514
Aghogho <monorienaghogho@gmail.com>
2023-07-06 22:23:44 +0000

    3. to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Convert float to str."""
+
+
+def to_str(n: float) -> str:
+    """
+    Args:
+      n(float) - floating input
+    Return: str repr of n
+    """
+    return str(n)
