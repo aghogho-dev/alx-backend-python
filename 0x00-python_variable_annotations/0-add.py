5a117842b4e090c7eef8333cb47ea8dcf8d30e41
Aghogho <monorienaghogho@gmail.com>
2023-07-06 21:58:06 +0000

    0. add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,10 +2,10 @@
 """Add two numbers."""
 
 def add(a: float, b: float) -> float:
-  """
-  Args:
-    a: float - first number
-    b: float - second number
-  Return: float
-  """
-  return a + b
+    """
+    Args:
+      a: float - first number
+      b: float - second number
+    Return: float
+    """
+    return a + b
